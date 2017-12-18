ada25257cb161419c44ed16440347600d3484342
seldon72 <ulloa.paco@gmail.com>
2017-12-18 14:43:19 -0600

    Adding projects page

--- server.js
+++ server.js
@@ -50,6 +50,13 @@ app.get('/about', (req, res) => {
   })
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+      pageTitle: 'Projects Page',
+      welcomeMessage: 'Projects Portafolio'
+  })
+});
+
 app.get('/bad', (req, res) => {
   res.send({
     errorMessage: 'This page really stinks',
